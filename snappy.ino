209579f3f2bb65683667ec3b74e72b3b4cbe016f
Shawn <shawn@gauntletllc.com>
2015-10-11 17:31:41 -0700

    Initial code commit

---
+++ snappy.ino
@@ -0,0 +1,36 @@
+// This #include statement was automatically added by the Particle IDE.
+#include "Robot.h"
+
+// This #include statement was automatically added by the Particle IDE.
+#include "Command.h"
+
+void setup()
+{
+    setupSteppers();
+    
+    // Listen for commands from the web!
+    Spark.function("command", processCommand);
+    
+    /*
+     * ==================================
+     * Add your commands below this line!
+     * ==================================
+     */
+    
+    addCommand(forward);
+    addCommand(back);
+    addCommand(left);
+    addCommand(right);
+}
+
+void loop()
+{
+    if(isDoneMoving() && commands.size())
+    {
+        runCommand(commands.front());
+        commands.pop();
+    }
+    
+    stepperLeft.run();
+    stepperRight.run();
+}
